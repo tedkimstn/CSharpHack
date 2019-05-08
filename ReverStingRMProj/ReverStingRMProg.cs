ff3b7cbe130accab8bc06a153c0ac14a0e1f2238
Ted Kim <tedkimflo@gmail.com>
2019-05-08 16:26:05 -0700

    c

--- ReverStingRMProj/ReverStingRMProg.cs
+++ ReverStingRMProj/ReverStingRMProg.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/*
+ * Source: 
+ * Definition Source (mdoc): https://docs.microsoft.com/en-us/dotnet/
+ * Author: Ted Kim
+ * Summary: 
+ * Modifications: N/A.
+ * Student: Ted Kim
+ * Capture Date: May 08, 2019
+ */
+
+using System;
 
 namespace ReverStingRMProj
 {
