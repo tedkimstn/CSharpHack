b9a1265d855e382f105876baeff3a212e42b7787
Ted Kim <tedkimflo@gmail.com>
2019-05-09 04:50:23 -0700

    c

---
+++ ReferenceMProj/ReferenceMProg.cs
@@ -0,0 +1,73 @@
+﻿/*
+ * Source: https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/ref
+ * Definition Source (mdoc): https://docs.microsoft.com/en-us/dotnet/
+ * Author: Microsoft Docs
+ * Summary: Pass an reference type argument and modify.
+ * Modifications: Added a namespace.
+ *                Moved methods into class Product.
+ *                Added a main method that calls ModifyProductsByReference.
+ * Student: Ted Kim
+ * Capture Date: May 09, 2019
+ */
+
+namespace ReferenceMProj
+{
+    class Product
+    {
+        static void Main(string[] args)
+        {
+            // Modify an item passed by reference.
+            ModifyProductsByReference();
+        }
+
+        // A constructor consisted of a name and an id.
+        public Product(string name, int newID)
+        {
+            ItemName = name;
+            ItemID = newID;
+        }
+
+        public string ItemName { get; set; }
+        public int ItemID { get; set; }
+
+        // Modifies a product passed by reference.
+        private static void ChangeByReference(ref Product itemRef)
+        {
+            // Replaces contents of itemRef from Fasteners to Stapler.
+            itemRef = new Product("Stapler", 99999);
+            // Replaces itemRef ItemID with 12345.
+            itemRef.ItemID = 12345;
+        }
+
+        // Creates a product, pass it onto another method by reference.
+        private static void ModifyProductsByReference()
+        {
+            // Instantiate a product.
+            Product item = new Product("Fasteners", 54321);
+            // Prints product values.
+            System.Console.WriteLine("Original values in Main.  Name: {0}, ID: {1}\n",
+                item.ItemName, item.ItemID);
+
+            // Another method changes the product pass by reference.
+            // Changes made are preserved as it was passed by a reference, not a value.
+            ChangeByReference(ref item);
+
+            // Prints changed product values.
+            System.Console.WriteLine("Back in Main.  Name: {0}, ID: {1}\n",
+                item.ItemName, item.ItemID);
+        }
+    }
+
+   
+}
+
+/* This code produces the following results:
+
+Original values in Main.  Name: Fasteners, ID: 54321
+
+Back in Main.  Name: Stapler, ID: 12345
+
+
+Press any key to continue...
+
+ */
---
+++ SealedMProj/SealedMProg.cs
@@ -0,0 +1,93 @@
+﻿/*
+ * Source: https://www.geeksforgeeks.org/c-sharp-sealed-class/
+ * Definition Source (mdoc): https://docs.microsoft.com/en-us/dotnet/
+ * Author: Akanksha_Rai
+ * Summary: Showcases sealed class members.
+ * Modifications: Added a namespace.
+ * Student: Ted Kim
+ * Capture Date: May 09, 2019
+ */
+
+using System;
+
+namespace SealedMProj
+{
+
+    class Printer
+    {
+        // Shows dimension.
+        // "virtual": "allow for it to be overridden in a derived class" (mdoc).
+        public virtual void show()
+        {
+            Console.WriteLine("display dimension : 6*6");
+        }
+
+        // Prints class name.
+        public virtual void print()
+        {
+            Console.WriteLine("printer printing....\n");
+        }
+    }
+
+    // LaserJet class inherits Printer class.
+    class LaserJet : Printer
+    {
+
+        // "sealed" Prevents derived classes from modifying it.
+        // "override": "extend or modify the abstract or virtual implementation 
+        //  of an inherited method" (mdoc).
+        sealed override public void show()
+        {
+            Console.WriteLine("display dimension : 12*12");
+        }
+
+        override public void print()
+        {
+            Console.WriteLine("Laserjet printer printing....\n");
+        }
+    }
+
+    class Officejet : LaserJet
+    {
+
+        override public void print()
+        {
+            Console.WriteLine("Officejet printer printing....");
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // Prints inherited and overridden methods.
+            Printer p = new Printer();
+            p.show();
+            p.print();
+
+            Printer ls = new LaserJet();
+            ls.show();
+            ls.print();
+
+            Printer of = new Officejet();
+            of.show();
+            of.print();
+        }
+    }
+}
+
+/* This code produces the following results:
+
+display dimension : 6*6
+printer printing....
+
+display dimension : 12*12
+Laserjet printer printing....
+
+display dimension : 12*12
+Officejet printer printing....
+
+Press any key to continue...
+
+ */
